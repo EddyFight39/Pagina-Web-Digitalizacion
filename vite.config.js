3550846adaebeb9231a91e1f84017b1e3ee434b9
EddyFight39 <87662601+EddyFight39@users.noreply.github.com>
2026-02-09 13:03:41 -0500

    Fix Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-está así import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Pagina-Web-Digitalizacion/' : '/',
+  base: '/Pagina-Web-Digitalizacion/',
   plugins: [react()],
 })
