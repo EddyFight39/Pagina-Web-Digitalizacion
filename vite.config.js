7997c7330610532a0585df1c043d95e361d21119
EddyFight39 <87662601+EddyFight39@users.noreply.github.com>
2026-02-09 12:55:44 -0500

    Update base path based on NODE_ENV

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
+está así import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Pagina-Web-Digitalizacion/',
+  base: process.env.NODE_ENV === 'production' ? '/Pagina-Web-Digitalizacion/' : '/',
   plugins: [react()],
 })
